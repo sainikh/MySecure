00a4b255b061b8538cf25c9cdb050f87c76b2c6d
Veeresh <veeresh@deskfactors.com>
2022-11-19 23:19:13 +0530

    MenuProvider is set in the Chasts Fragmnet. implements methods and inflate it in the video bellow explain how this works https://www.youtube.com/watch?v=xPzI0EPP07g&t=436s

--- app/src/main/java/com/example/mysecure/Chats_Fragment.kt
+++ app/src/main/java/com/example/mysecure/Chats_Fragment.kt
@@ -1,23 +1,63 @@
 package com.example.mysecure
 
 import android.os.Bundle
+import android.view.*
+import android.widget.Toast
+import androidx.core.view.MenuProvider
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import androidx.lifecycle.Lifecycle
+import com.example.mysecure.databinding.FragmentChatsBinding
 
 
-class Chats_Fragment : Fragment() {
+class Chats_Fragment : Fragment(),MenuProvider {
 
+    private var _binding : FragmentChatsBinding? = null
+    private val binding : FragmentChatsBinding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_chats_, container, false)
+
+        _binding = FragmentChatsBinding.inflate(inflater,container,false)
+
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        activity?.addMenuProvider(this,viewLifecycleOwner,Lifecycle.State.RESUMED)
     }
 
 
+    override fun onCreateMenu(menu: Menu, menuInflater: MenuInflater) {
+        menuInflater.inflate(R.menu.mainmenu,menu)
+    }
+
+    override fun onMenuItemSelected(menuItem: MenuItem): Boolean {
+        if(menuItem.itemId == R.id.item1)
+        {
+            Toast.makeText(requireContext(),"Clicked Serch Option in Menu",Toast.LENGTH_SHORT).show()
+            return true
+        }
+        if(menuItem.itemId == R.id.item2)
+        {
+            Toast.makeText(requireContext(),"Create Group Clicked",Toast.LENGTH_SHORT).show()
+            return true
+        }
+        if(menuItem.itemId == R.id.item3)
+        {
+            Toast.makeText(requireContext(),"Create BroadCast Clicked",Toast.LENGTH_SHORT).show()
+            return true
+        }
+
+        return false
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
+    }
 
 }
